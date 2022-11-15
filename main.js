ca2f6753fffec4c3b863c805a617097ab8ca334c
Isaac Alves Pinheiro <61624336+IsaacAlves7@users.noreply.github.com>
2022-11-15 18:32:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,107 @@
+// Hearts - jQuery
+document.addEventListener("mousemove", function(e){ // funcão de evento movimento do mouse
+    let body = document.querySelector('body'); // selecionar em todo o corpo
+    let heart = document.createElement('article'); // criar um elemento span
+    let x = e.offsetX; // coordenada X dentro do corpo
+    let y = e.offsetY; // coordenada Y dentro do corpo
+    heart.style.left = x+'px'; // estilo de movimento por px no X
+    heart.style.top = y+'px'; // estilo de movimento por px no Y
+
+    let size = Math.random() * 80;
+    heart.style.width = 20 + size + 'px';
+    heart.style.height = 20 + size + 'px';
+
+    let transformValue = Math.random() * 360;
+    heart.style.transform = 'rotate('+ transformValue + 'deg)';
+
+    body.appendChild(heart); //corpo heranca de filho por coracao
+
+    setTimeout(function(){ // definindo o tempo para remover os coracoes filhos
+        heart.remove();
+    },1000)
+})
+
+// Flipped Animation
+// References to DOM Elements
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+// Event Listener
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+
+// Business Logic
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translateX(180px)";
+}
+
+function closeBook(isAtBeginning) {
+    if(isAtBeginning) {
+        book.style.transform = "translateX(0%)";
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+    
+    prevBtn.style.transform = "translateX(0px)";
+    nextBtn.style.transform = "translateX(0px)";
+}
+
+function goNextPage() {
+    if(currentLocation < maxLocation) {
+        switch(currentLocation) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                closeBook(false);
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+        currentLocation++;
+    }
+}
+
+function goPrevPage() {
+    if(currentLocation > 1) {
+        switch(currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+
+        currentLocation--;
+    }
+}
